99ad740998169acad1c185dec37bf412cfc9b27d
morisamon <amonmoris@gmail.com>
2018-01-15 21:46:19 +0200

    added images

---
+++ Drive2StudyApp/CircleObject.swift
@@ -0,0 +1,27 @@
+//
+//  CircleObject.swift
+//  Drive2StudyApp
+//
+//  Created by admin on 09/01/2018.
+//  Copyright © 2018 IdoD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CircleObject{
+    
+    static let instance = CircleObject()
+    
+    func circleImageView (object: UIImageView){
+        object.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        object.layer.cornerRadius = 0.5 * object.bounds.size.width
+        object.clipsToBounds = true
+    }
+    
+    func circleButton (object: UIButton){
+        object.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        object.layer.cornerRadius = 0.5 * object.bounds.size.width
+        object.clipsToBounds = true
+    }
+}
